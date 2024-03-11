47ee1d97adf73c9228f796d621afb11bd50c7ad2
David Unger <Davidunger@outlook.com>
2024-03-11 11:51:35 +0100

    inital commit

---
+++ src/routes/testComponent.svelte
@@ -0,0 +1,13 @@
+<script>
+	let name = "Svelte";
+    
+</script>
+<h1>Hello {name}!</h1>
+
+<img src="/my-svelte-api-app/static/img/1.jpg" alt="test"/>
+<style>
+    h1 {
+        color:red;
+        font-size: 2em;
+        } 	
+</style>
